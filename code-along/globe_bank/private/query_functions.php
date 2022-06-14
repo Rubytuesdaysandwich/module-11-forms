189beb1cc12bae9f65611947acae9e5e13e8387b
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2022-06-14 12:44:48 -0600

    update

--- code-along/globe_bank/private/query_functions.php
+++ code-along/globe_bank/private/query_functions.php
@@ -25,6 +25,36 @@
     return $subject; // returns an assoc. array
   }
 
+  function validate_subject($subject) {
+    $errors = [];
+
+    // menu_name
+    if(is_blank($subject['menu_name'])) {
+      $errors[] = "Name cannot be blank.";
+    } elseif(!has_length($subject['menu_name'], ['min' => 2, 'max' => 255])) {
+      $errors[] = "Name must be between 2 and 255 characters.";
+    }
+
+    // position
+    // Make sure we are working with an integer
+    $postion_int = (int) $subject['position'];
+    if($postion_int <= 0) {
+      $errors[] = "Position must be greater than zero.";
+    }
+    if($postion_int > 999) {
+      $errors[] = "Position must be less than 999.";
+    }
+
+    // visible
+    // Make sure we are working with a string
+    $visible_str = (string) $subject['visible'];
+    if(!has_inclusion_of($visible_str, ["0","1"])) {
+      $errors[] = "Visible must be true or false.";
+    }
+
+    return $errors;
+  }
+  
   function insert_subject($subject) {
     global $db;
 
