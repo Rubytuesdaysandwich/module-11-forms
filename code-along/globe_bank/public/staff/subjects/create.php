738e380f308108c6ea0f10de44e1ccec9510d365
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2022-06-14 16:25:55 -0600

    update

--- code-along/globe_bank/public/staff/subjects/create.php
+++ code-along/globe_bank/public/staff/subjects/create.php
@@ -4,15 +4,21 @@ require_once('../../../private/initialize.php');
 if(is_post_request()){//checking to see if it is a post request. If it is a get request we get redirected to staff/subjects/new.php'
 // Handle form values sent by new.php
 
-    $menu_name = $_POST['menu_name'] ?? '';
-    $position = $_POST['position'] ?? '';
-    $visible = $_POST['visible'] ?? '';
+$subject = [];
+$subject['id'] = $id;//id of the subject
+$subject['menu_name'] = $_POST['menu_name'] ?? '';//post to menu_name
+$subject['position'] = $_POST['position'] ?? '';//post to position
+$subject['visible'] = $_POST['visible'] ?? '';//post to visible
 
-    echo "Form parameters<br />";
-    echo "Menu name: " . $menu_name . "<br />";
-    echo "Position: " . $position . "<br />";
-    echo "Visible: " . $visible . "<br />";
-}else {
-    redirect_to(url_for('staff/subjects/new.php'));
+$result = insert_subject($subject);//insert subject
+if($result===true){
+   $new_id = mysqli_insert_id($db);//create a new id
+   redirect_to(url_for('/staff/subjects/show.php?id='.$new_id));
+} else{
+    $errors = $result;//if not it will return errors to the user
 }
-?>
+
+}else {
+    redirect_to(url_for('staff/subjects/new.php'));//redirect to staff/subjects/new.php
+}
+// ?>
--- code-along/globe_bank/public/staff/subjects/new.php
+++ code-along/globe_bank/public/staff/subjects/new.php
@@ -2,15 +2,29 @@
 //!white space like this is ok
 $test = $_GET['test']?? '';
 
-if($test =='404'){//test if 404
-    error_404();
-}elseif($test == '500'){//test if 500
-    error_500();
-}
-elseif($test=='redirect'){//test if redirect
-redirect_to(url_for('staff/subjects/index.php'));
-}
-?>
+if(is_post_request()){//checking to see if it is a post request. If it is a get request we get redirected to staff/subjects/new.php'
+  // Handle form values sent by new.php
+  
+  $subject = [];
+  $subject['id'] = $id;//id of the subject
+  $subject['menu_name'] = $_POST['menu_name'] ?? '';//post to menu_name
+  $subject['position'] = $_POST['position'] ?? '';//post to position
+  $subject['visible'] = $_POST['visible'] ?? '';//post to visible
+  
+  $result = insert_subject($subject);//insert subject
+  if($result===true){
+     $new_id = mysqli_insert_id($db);//create a new id
+     redirect_to(url_for('/staff/subjects/show.php?id='.$new_id));
+  } else{
+      $errors = $result;//if not it will return errors to the user
+  }
+  
+  }else {
+      
+  }
+  ?>
+  
+
 
 <?php $page_title = 'Create Subject'; ?>
 <?php include(SHARED_PATH . '/staff_header.php');//path to header html ?>
@@ -22,7 +36,9 @@ redirect_to(url_for('staff/subjects/index.php'));
   <div class="subject new">
     <h1>Create Subject</h1>
 
-    <form action="<?php echo url_for('staff/subjects/create.php')?>" method="post"><!--start form for new create form-->
+  <?php echo display_errors($errors);?>
+
+    <form action="<?php echo url_for('staff/subjects/new.php')?>" method="post"><!--start form for new create form-->
       <dl>
         <dt>Menu Name</dt>
         <dd><input type="text" name="menu_name" value="" /></dd><!--input for menu name-->
@@ -31,7 +47,11 @@ redirect_to(url_for('staff/subjects/index.php'));
         <dt>Position</dt>
         <dd>
           <select name="position"><!--get position-->
+            <option value="0">0</option>
             <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
           </select>
         </dd>
       </dl>
